c5dd5b4bd268637c59707f5641dca1c7971dfb31
Avery Walker <spikypancakedragon@gmail.com>
2024-03-27 22:10:47 -0500

    initial commit, added problem 1

---
+++ prob1.rb
@@ -0,0 +1,14 @@
+require 'httparty'
+require 'json'
+
+response = HTTParty.get('http://api.weatherapi.com/v1/history.json?q=38133&dt=2024-03-26&key= b1f7372db07c43cfbee23040242803')
+
+if response.code == 200
+  dictionary = JSON.parse(response.body)
+  sum = 0
+  for i in 0..23
+    sum += dictionary["forecast"]["forecastday"][0]["hour"][i]["temp_f"]
+  end
+  avg = sum / 24
+  puts "Average temperature for 2024-03-26: #{avg.round(2)}F"
+end
